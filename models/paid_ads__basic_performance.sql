8d319545603ada011971192e654b7499e9ad2931
AleksandraBezborodova <78304910+AleksandraBezborodova@users.noreply.github.com>
2023-06-18 22:04:54 +0300

    Update paid_ads__basic_performance.sql

--- models/paid_ads__basic_performance.sql
+++ models/paid_ads__basic_performance.sql
@@ -5,3 +5,13 @@ union all
 
 select *
 from {{ ref('stg_creative_facebook_ads_data') }}
+
+union all
+
+select *
+from {{ ref('stg_tiktok_ads_data') }}
+
+union all
+
+select *
+from {{ ref('stg_promoted_tweets_twitter_data') }}
