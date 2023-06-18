14c04c2952bd6e2cc6e80701e6fe82a9de5ec6ed
AleksandraBezborodova <78304910+AleksandraBezborodova@users.noreply.github.com>
2023-06-18 21:17:56 +0300

    Update cost_per_engage.sql

--- analyses/cost_per_engage.sql
+++ analyses/cost_per_engage.sql
@@ -1,3 +1,3 @@
 select channel, safe_divide(sum(spend), sum(engagements)) as cost_per_engage
-from {{ ref('paid_ads__basic_performance.sql') }}
+from {{ ref('paid_ads__basic_performance') }}
 group by channel
